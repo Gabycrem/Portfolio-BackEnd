3ef29ec570f8744960ede5165c172ae859c3e126
gabycrem <gabymacre@gmail.com>
2022-10-31 21:44:22 -0300

    Educacion

---
+++ src/main/java/com/portfolio/nm/Repository/REducacion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.nm.Repository;
+
+import com.portfolio.nm.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Agustin
+ */
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
